756ff0a59bd1dd13a19c486bf628e8867fa8fdcd
Ali Hadizadeh <ali@hadizadeh.com>
2022-08-20 12:59:18 -0400

    game logic  working

--- Player.py
+++ Player.py
@@ -1,16 +1,27 @@
 class Player:
+    HELD_CARDS_NUMBER = 1
+
     def __init__(self, name) -> None:
         self.name = name
         self._hand = []
+        self.heldCards = []
 
     def playCard(self):
-        print(f'{self._hand[0][1]} of {self._hand[0][0]}')
+        return self._hand.pop(0)
 
     def receiveCard(self, card):
         self._hand.append(card)
 
     def hand(self):
-        return self._hand
+        return len(self._hand)
+
+    def putCardsOnHold(self, numberOfCardsToBeHeld=HELD_CARDS_NUMBER):
+        tempCard = self._hand[0]
+        self._hand.pop(0)
+        return tempCard
+
+    def receiveHeldCards(self, hd):
+        self._hand.extend(hd)
 
     @property
     def name(self):
@@ -19,3 +30,9 @@ class Player:
     @name.setter
     def name(self, name):
         self._name = name
+
+    def continuePlay(self):
+        return len(self._hand) > 0
+
+    def cardCount(self):
+        return len(self.hand)
--- main.py
+++ main.py
@@ -10,5 +10,49 @@ player2 = Player("Player2")
 
 deckOfCards.deal(player1, player2)
 
-player1.playCard()
-player2.playCard()
+cardsOnTable = []
+
+while (player1.hand() > 0) and (player2.hand() > 0):
+    player1Card = player1.playCard()
+    player2Card = player2.playCard()
+    print(f'{player1.name} has {player1Card[1]} of {player1Card[0]}')
+    print(f'{player2.name} has {player2Card[1]} of {player2Card[0]}')
+    if player1Card[2] > player2Card[2]:
+        print(f'{player1.name} get the cards')
+        player1.receiveCard(player1Card)
+        player1.receiveCard(player2Card)
+        if len(cardsOnTable) > 0:
+            player1.receiveHeldCards(cardsOnTable)
+            cardsOnTable.clear()
+    elif player1Card[2] < player2Card[2]:
+        print(f'{player2.name} get the cards')
+        player2.receiveCard(player2Card)
+        player2.receiveCard(player1Card)
+        if len(cardsOnTable) > 0:
+            player2.receiveHeldCards(cardsOnTable)
+            cardsOnTable.clear()
+    elif player1Card[2] == player2Card[2]:
+        if (player2.hand() > 0) and (player1.hand() > 0):
+            print(f'cards are equal. Set aside the cards')
+            cardsOnTable.append(player1.putCardsOnHold())
+            print(f'after 1 ====== {cardsOnTable}')
+            cardsOnTable.append(player2.putCardsOnHold())
+            print(f'after 2 ====== {cardsOnTable}')
+            cardsOnTable.append(player1Card)
+            cardsOnTable.append(player2Card)
+        elif (player1.hand() == 0) or (player2.hand() == 0):
+            if player2.hand() == 0:
+                print(f'{player1.name} has won the game')
+                print(f'{player1.name} Card Count: {player1.hand()}')
+                print(f'{player2.name} Card Count: {player2.hand()}')
+            else:
+                print(f'{player2.name} has won the game')
+                print(f'{player1.name} Card Count: {player1.hand()}')
+                print(f'{player2.name} Card Count: {player2.hand()}')
+    input('Press enter to continue: ')
+print(f'{player1.name} Card Count: {player1.hand()}')
+print(f'{player2.name} Card Count: {player2.hand()}')
+if player1.hand() == 52:
+    print(f'{player1.name} has won the game')
+if player2.hand() == 52:
+    print(f'{player2.name} has won the game')
